d0a0ff0b90b77548fbbacceb2abf8fb12b9c9e83
SH-YJ <2973211152@qq.com>
2020-11-13 15:01:07 +0800

    update

--- Tkinter 图型界面/Sprider_pic_tx.py
+++ Tkinter 图型界面/Sprider_pic_tx.py
@@ -21,30 +21,33 @@ def download():
     html_url = getUrl(entry1.get())
     all_url = re.findall('<a href="(.*?)" class="img" target=".*?">', html_url)
     for url in all_url:
-        I_url = 'https://www.woyaogexing.com/' + url
-        II_url = getUrl(I_url)
-        soup = BeautifulSoup(II_url, "html.parser")
-        file_name = re.findall('<h1>(.*?)</h1>', II_url)[-1]
-        all_img = soup.find('ul', class_='artCont cl').find_all('img')
-        for img in all_img:
-            src = img['src']
-            img_url = 'https:' + src
-            root = entry3.get() + str(file_name) + '/'  # 保存的路径
-            path = root + img_url.split('/')[-1]  # 获取img的文件名
-            print(path)
-            try:
-                if not os.path.exists(root):  # 判断是否存在文件并下载img
-                    os.mkdir(root)
-                if not os.path.exists(path):
-                    read = requests.get(img_url)
-                    with open(path, "wb")as f:
-                        f.write(read.content)
-                        f.close()
-                        print("文件保存成功！")
-                else:
-                    print("文件已存在！")
-            except:
-                print("文件爬取失败！")
+        try:
+            I_url = 'https://www.woyaogexing.com/' + url
+            II_url = getUrl(I_url)
+            soup = BeautifulSoup(II_url, "html.parser")
+            file_name = re.findall('<h1>(.*?)</h1>', II_url)[-1]
+            all_img = soup.find('ul', class_='artCont cl').find_all('img')
+            for img in all_img:
+                src = img['src']
+                img_url = 'https:' + src
+                root = entry3.get() + str(file_name) + '/'  # 保存的路径
+                path = root + img_url.split('/')[-1]  # 获取img的文件名
+                print(path)
+                try:
+                    if not os.path.exists(root):  # 判断是否存在文件并下载img
+                        os.mkdir(root)
+                    if not os.path.exists(path):
+                        read = requests.get(img_url)
+                        with open(path, "wb")as f:
+                            f.write(read.content)
+                            f.close()
+                            print("文件保存成功！")
+                    else:
+                        print("文件已存在！")
+                except:
+                    print("文件爬取失败！")
+        except:
+            print("未找到图片网址")
     messagebox.showinfo('提示', '下载完成')
 
 
