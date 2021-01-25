0631fe960cae2aba8cf57a77b0ed1c51b691ea2b
SH-YJ <2973211152@qq.com>
2021-01-25 13:49:25 +0800

    update

--- Normal/Study/List.py
+++ Normal/Study/List.py
@@ -1,9 +1,6 @@
 from math import *
 import random
 
-a = f'{1 + 3}'
-print(type(a))
-
 str = '1'
 print(str.isdigit())
 
@@ -12,4 +9,6 @@ s2 = '12345135431256'
 # str1 = 'google'
 # seq = ('g', 'o', 'o', 'g', 'l', 'e')
 # a = str1.maketrans(s1, s2)
-print(s2.rindex('1',0,len(s2)))
+print('ab c\n\nde fg\rkl\r\n'.splitlines(True))
+list = [1, 2, 3, 4, 5]
+
